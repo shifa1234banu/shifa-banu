c70f389b7024873817bac95ad92bfd89c51ddcd5
shifa1234banu <shifabanum212@gmail.com>
2021-04-22 00:58:34 +0530

    Final 2.3

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-$(document).ready(function () {
+$(document).ready(function(){
     $("#submit-form").validate({
-        rules: {
-            Name: {
-                required: true,
-                minlength: 4,
-            },
-            Place:{
-                required:true,
-                
-            },
-            Email:{
-                required:true,
-                email:true
-            },
-            Phone:{
-                required:true,
-                number:true,
-                minlength:10,
-                maxlength:10
-            }
+        rules:{
+        Name:{
+            required:true,
+            minlength:4
+        },
+        Place:{
+            required:true,
+            minlength:4
+
+        },
+        Email:{
+            required:true,
+            email:true
+        },
+        Phone:{
+            required:true,
+            number:true,
+            minlength:10,
+            maxlength:10
         }
+
+    },
+    messages:{
+        
+}
+
     })
 })
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
